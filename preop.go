07a3352ee8282e308295459ed0022e281113d46f
carter2000 <zarcardfly@gmail.com>
2012-10-30 17:50:40 +0800

    save

--- preop.go
+++ preop.go
@@ -14,28 +14,30 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
-	"qbox.us/cc/config"
+	qconfig "qbox.us/cc/config"
 	"qbox.us/log"
+	"qbox.us/rpc"
 )
 
 var (
-	confName = flag.String("t", "preop.conf", "task file")
+	confName = flag.String("c", "preop.conf", "config file")
 )
 
 const (
 	logPrefix = "data_"
 )
 
-type task struct {
+type config struct {
 	IODomain	string	`json:"io_domain"`
 	LogIndex	int	`json:"log_index"`
 	Params		[]string`json:"params"`
 	Off		int64	`json:"off"`
 	IntervalNS	int64	`json:"interval_ns"`
 	MaxLogSize	int64	`json:"max_logsize`
+	Uids		[]uint32`json:"uids"`
 	Follow		bool	`json:"follow"`
 	Save		bool	`json:"save"`
-	Debug		int	`json:"debug_level"`
+	DebugLevel	int	`json:"debug_level"`
 	RW		sync.RWMutex	`json:"-"`
 }
 
@@ -43,17 +45,17 @@ func main() {
 
 	flag.Parse()
 
-	var conf task
-	if err := config.LoadEx(&conf, *confName); err != nil {
+	var conf config
+	if err := qconfig.LoadEx(&conf, *confName); err != nil {
 		return
 	}
 
-	log.SetOutputLevel(conf.Debug)
+	log.SetOutputLevel(conf.DebugLevel)
 
 	logName := logPrefix + strconv.Itoa(conf.LogIndex)
 	logFile, err := os.Open(logName)
 	if err != nil {
-		log.Fatal("open", conf.Log, "fail:", err)
+		log.Fatal("open ", logName, " fail: ", err)
 	}
 	defer func() {
 		logFile.Close()
@@ -63,8 +65,8 @@ func main() {
 	go interruptSave(&conf)
 	go successiveSave(&conf)
 
-	logFile.Seek(conf.Off, os.SEEK_SET)
-	reader := bufio.NewReader(logFile)
+	var reader *bufio.Reader
+	reader, conf.Off = getReader(logFile, conf.Off)
 
 	line := make([]byte, 0, 128)
 	now := time.Now()
@@ -82,30 +84,126 @@ func main() {
 				break
 			}
 
-			if newFile, ok := reOpen(logFile, conf.MaxLogSize); ok {
-				logFile.Close()
-				if logFile.Name() != newFile.Name() {
-					conf.RW.Lock()
-					conf.Off = 0
-					conf.LogIndex = getLogIndex(newFile.Name())
-					conf.RW.Unlock()
-				}
-				logFile = newFile
-				logFile.Seek(conf.Off, os.SEEK_SET)
-				reader = bufio.NewReader(logFile)
+			newFile := reOpen(logFile, conf.MaxLogSize)
+			if newFile == nil {
+				continue
 			}
+
+			if logFile.Name() != newFile.Name() {
+				conf.RW.Lock()
+				conf.Off = 0
+				conf.LogIndex = getLogIndex(newFile.Name())
+				conf.RW.Unlock()
+				log.Debug("reOpen ", newFile.Name())
+			}
+			logFile.Close()
+			logFile = newFile
+
+			reader, conf.Off = getReader(logFile, conf.Off)
 
 			continue
 		}
 
 		atomic.AddInt64(&conf.Off, int64(len(line)) + 1)
 
-		if uri, ok := parseLine(string(line); ok {
+		if uri, ok := parseLine(string(line), conf.Uids); ok {
 			preOp(conf.IODomain, string(uri), conf.Params, conf.Save)
 		}
 
 		line = line[:0]
 	}
+}
+
+func parseLine(line string, uids []uint32) (uri string, ok bool) {
+
+	const (
+		RS_REQ = "REQ\tRS\t"
+		RS_PUT = "/put/"
+		CONTENT_TYPE = "\"Content-Type\":"
+		TOKEN_UID = "\"uid\":"
+	)
+
+	if !strings.HasPrefix(line, RS_REQ) {
+		return
+	}
+
+	words := strings.SplitN(line[len(RS_REQ):], "\t", 6)
+	if len(words) < 6 {
+		return
+	}
+
+	if words[1] != "POST" || words[4] != "200" {
+		return
+	}
+
+	url := words[2]
+	if !strings.HasPrefix(url, RS_PUT) {
+		return
+	}
+
+	entry := url[len(RS_PUT):]
+	if i := strings.Index(entry, "/"); i >= 0 {
+		entry = entry[:i]
+	}
+	log.Debug("parse entry: ", entry)
+
+	header := words[3]
+	ctIdx := strings.Index(header, CONTENT_TYPE)
+	if ctIdx < 0 {
+		return
+	}
+
+	if !strings.HasPrefix(header[ctIdx + len(CONTENT_TYPE) + 1:], "\"image/") {
+		return
+	}
+
+	uidIdx := strings.Index(header, TOKEN_UID)
+	if uidIdx < 0 {
+		return
+	}
+
+	strUid := header[uidIdx + len(TOKEN_UID) + 1:]
+	if i := strings.Index(strUid, ","); i < 0 {
+		strUid = strUid[:i]
+	}
+
+	uid, err := strconv.ParseUint(strUid, 10, 32)
+	if err != nil {
+		return
+	}
+
+	log.Debug("parse uid: ", uid)
+	if !contain(uids, uint32(uid)) {
+		return
+	}
+
+	entryURI, err := rpc.DecodeURI(entry)
+	if err != nil {
+		log.Warn(entryURI, " decode fail: ", err)
+		return
+	}
+
+	uri = strings.Replace(entryURI, ":", "/", 1)
+	return uri, true
+}
+
+func contain(a []uint32, t uint32) bool {
+
+	for _, v := range a {
+		if v == t {
+			return true
+		}
+	}
+	return false
+}
+
+func getReader(f *os.File, off int64) (*bufio.Reader, int64) {
+
+	newOff, err := f.Seek(off, os.SEEK_SET)
+	if err != nil {
+		log.Warn("Seek fail: ", err)
+	}
+	return bufio.NewReader(f), newOff
 }
 
 func getLogIndex(name string) int {
@@ -134,11 +232,11 @@ func getNextLog(name string) *os.File {
 		return nil
 	}
 
-	closestF := nil
+	var closestF *os.File
 	closestIdx := -1
 	minIdx := getLogIndex(name)
 	for _, fn := range fns {
-		idx := GetLogIndex(fn.Name())
+		idx := getLogIndex(fn)
 		if idx <= minIdx {
 			continue
 		}
@@ -148,7 +246,7 @@ func getNextLog(name string) *os.File {
 			continue
 		}
 
-		if closestF != nil && closetIdx < idx {
+		if closestF != nil && closestIdx < idx {
 			continue
 		}
 
@@ -171,44 +269,42 @@ func sleep(now, last time.Time, tick int64) {
 	}
 }
 
-func reOpen(f *os.File, maxSize int64) (*os.File, bool) {
+func reOpen(f *os.File, maxSize int64) (nf *os.File) {
 
 	oldFi, err := f.Stat()
 	if err != nil {
 		log.Warn("stat", err)
-		return oldF, false
+		return
 	}
 
 	if oldFi.Size() >= maxSize {
-		if nf := getNextLog(f); nf != nil {
-			return nf, true
-		}
-		return f, false
+		return getNextLog(f.Name())
 	}
 
 	newFi, err := os.Stat(f.Name())
 	if err != nil {
 		log.Warn("stat", err)
-		return oldF, false
+		return
 	}
 
 	if newFi.Size() == oldFi.Size() {
-		return oldF, false
+		return
 	}
 
-	nf, err := os.Open(f.Name())
+	f2, err := os.Open(f.Name())
 	if err != nil {
 		log.Warn("open", f.Name(), "fail:", err)
+		return
 	}
 
-	return nf, true
+	return f2
 }
 
-func save(t *task) {
+func save(conf *config) {
 
-	t.RW.RLock()
-	b, err := json.MarshalIndent(t, "", "\t")
-	t.RW.RUnlock()
+	conf.RW.RLock()
+	b, err := json.MarshalIndent(conf, "", "\t")
+	conf.RW.RUnlock()
 	if err != nil {
 		log.Warn("marshal:", err)
 		return
@@ -227,26 +323,25 @@ func save(t *task) {
 	return
 }
 
-func interruptSave(t *task) {
+func interruptSave(conf *config) {
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	for _ = range c {
-		save(t)
+		save(conf)
 		os.Exit(0)
 	}
 }
 
-func successiveSave(t *task) {
+func successiveSave(conf *config) {
 
 	c := time.Tick(time.Minute)
 	for _ = range c {
-		save(t)
+		save(conf)
 	}
 }
 
 func preOp(host string, uri string, params []string, save bool) {
-
 
 	for i,p := range params {
 		url := host + "/" + uri + p
